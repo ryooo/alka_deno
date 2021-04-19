150f776a1de796241208e6a360a54d92293b9056
ryooo <ryooo.321@gmail.com>
2021-04-19 14:30:27 +0900

    fix gtm link

--- app.tsx
+++ app.tsx
@@ -1,24 +1,30 @@
 import { useDeno } from 'framework/react'
-import React, { ComponentType } from 'react'
+import React, { ComponentType, useEffect } from 'react'
 import Header from './components/header.tsx'
 
 export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
+
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src = "https://www.googletagmanager.com/gtag/js?id=GTM-KPXJ2GB"
+    script.async = true
+    document.body.appendChild(script)
+    const setupScript = document.createElement("script")
+    setupScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){
+        dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+    `
+    document.body.appendChild(setupScript)
+  }, [])
 
   return (
     <main>
       <link rel="stylesheet" href="./style/app.css" />
       <Header />
       <Page {...pageProps} />
-      <>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=default`}></script>
-        <script>{`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){
-              dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-          `}</script>
-      </>
     </main>
   )
 }
