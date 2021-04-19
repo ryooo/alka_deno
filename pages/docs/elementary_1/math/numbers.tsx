e6359374c5199b51d897a72843844aba61156ade
ryooo <ryooo.321@gmail.com>
2021-04-19 17:26:36 +0900

    change indent 2

--- pages/docs/elementary_1/math/numbers.tsx
+++ pages/docs/elementary_1/math/numbers.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { getTitle } from '~/components/navList.tsx'
 
 export default function PageMainContents(props) {
-    return (
-        <h1>Hello, bba</h1>
-    )
+  return (
+    <h1>Hello, bba</h1>
+  )
 }
 
 PageMainContents.meta = {
-    title: getTitle(import.meta.url),
+  title: getTitle(import.meta.url),
 }
